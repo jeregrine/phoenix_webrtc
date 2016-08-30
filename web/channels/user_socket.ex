a749c07e14a90290167d34931a3833950abb2fb6
Jason Stiebs <jason@rokkincat.com>
2016-08-30 12:33:19 -0500

    heroku stuff

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -7,7 +7,7 @@ defmodule PhoenixWebrtc.UserSocket do
   channel "call:*", PhoenixWebrtc.CallChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
